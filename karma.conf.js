3fedb3d7873bc614e0663e2fdafe7ba68242053c
Keith Byrne <keithmbyrne@gmail.com>
2019-07-04 01:32:12 +0100

    travis and karma conf changes

--- karma.conf.js
+++ karma.conf.js
@@ -5,82 +5,86 @@ var path = require('path');
 var webpackConfig = require('./webpack.config.js');
 
 module.exports = function(config) {
-    config.set({
+    config.set(
+        {
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
+            basePath: '',
 
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+            // frameworks to use
+            // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+            frameworks: ['jasmine'],
 
-        // list of files / patterns to load in the browser
-        files: [
-            'tests/**/*.test.js',
-            'src/**/*.test.js',
-        ],
+            // list of files / patterns to load in the browser
+            files: [
+                'tests/**/*.test.js',
+                'src/**/*.test.js',
+            ],
 
-        // list of files / patterns to exclude
-        exclude: [],
+            // list of files / patterns to exclude
+            exclude: [],
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
+            // preprocess matching files before serving them to the browser
+            // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+            preprocessors:
+        {
             'test/**/*test.js': ['webpack'],
             'test/**/*test.jsx': ['webpack'],
             'src/**/*test.js': ['webpack'],
             'src/**/*test.jsx': ['webpack']
         },
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+            // test results reporter to use
+            // possible values: 'dots', 'progress'
+            // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+            reporters: ['progress'],
 
-        // web server port
-        port: 9876,
+            // web server port
+            port: 9876,
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+            // enable / disable colors in the output (reporters and logs)
+            colors: true,
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+            // level of logging
+            // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+            logLevel: config.LOG_INFO,
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+            // enable / disable watching file and executing tests whenever any file changes
+            autoWatch: true,
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+            // start these browsers
+            // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+            browsers: ['ChromeHeadless'],
 
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
+            // Continuous Integration mode
+            // if true, Karma captures browsers, runs the tests and exits
+            singleRun: false,
 
-        // Concurrency level
-        // how many browser should be started simultaneous
-        concurrency: Infinity,
+            // Concurrency level
+            // how many browser should be started simultaneous
+            concurrency: Infinity,
 
-        coverageReporter: {
-            reporters: [{
+            coverageReporter:
+        {
+            reporters: [
+                {
                     type: 'text'
                 },
                 {
                     type: 'html'
-                }
-            ]
+                }]
         },
 
-        webpack: webpackConfig,
+            webpack: webpackConfig,
 
-        reporters: ['progress', 'coverage-istanbul'],
+            reporters: ['progress', 'coverage-istanbul'],
 
-        // any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
-        coverageIstanbulReporter: {
+            // any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
+            coverageIstanbulReporter:
+        {
             // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
             reports: ['html'],
             fixWebpackSourcePaths: true
         }
-    });
-}
+        });
+};
