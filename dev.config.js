a528e847bf23c514119e7c347449cdf8ac576ac5
Keith Byrne <kmjbyrne@amazon.com>
2019-07-26 22:40:24 +0100

    update config and webpack

--- dev.config.js
+++ dev.config.js
@@ -6,15 +6,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = function init() {
     let config = {};
     config.entry = {
-        app: ['./src/index.js', './src/main.scss'],
-        vendor: ['./src/vendor.js', './src/vendor.scss']
+        app: ['./src/index.ts', './src/main.scss'],
+        vendor: ['./src/vendor.js', './src/vendor.scss'],
     };
     config.output = {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js'
     };
     config.resolve = {
-        extensions: ['.js', '.css', '.scss', '.html', '.json']
+        extensions: ['.ts', '.js', '.css', '.scss', '.html', '.json']
     };
     config.devtool = 'inline-source-map';
     config.devServer = {
@@ -23,8 +23,13 @@ module.exports = function init() {
     };
     config.watch = true;
     config.plugins = [
+        // new HtmlWebpackPlugin({
+        //     template: path.resolve(__dirname, 'src/', 'index.html'),
+        //     filename: './index.html',
+        //     hash: true
+        // }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/', 'index.html'),
+            template: path.resolve(__dirname, 'src/', 'dashboard.html'),
             filename: './index.html',
             hash: true
         }),
@@ -35,34 +40,39 @@ module.exports = function init() {
     ];
     config.module = {
         rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: path.resolve(__dirname, 'node_modules'),
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        },
-        {
-            test: /\.(sa|sc|c)ss$/,
-            use: [{
-                loader: MiniCssExtractPlugin.loader,
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: path.resolve(__dirname, 'node_modules'),
                 options: {
-                    hmr: true,
-                    reloadAll: true,
-                },
-            },
-            'css-loader', 'postcss-loader', {
-                loader: 'sass-loader',
-                options: {
-                    sourceMap: true
+                    presets: ['@babel/preset-env']
                 }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: path.resolve(__dirname, 'node_modules')
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: true,
+                            reloadAll: true,
+                        },
+                    },
+                    'css-loader', 'postcss-loader', {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader'
             }
-            ],
-        },
-        {
-            test: /\.(png|jpg)$/,
-            loader: 'url-loader'
-        }
         ]
     };
     return config;
--- prod.config.js
+++ prod.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const path = require('path');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -6,18 +7,21 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = function init() {
     let config = {};
     config.entry = {
-        app: ['./src/index.js', './src/main.scss'],
+        app: ['./src/index.ts', './src/main.scss'],
         vendor: ['./src/vendor.js', './src/vendor.scss']
     };
     config.output = {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
+        // library: 'AmzUi',
+        // libraryTarget: 'umd'
     };
     config.resolve = {
-        extensions: ['.js', '.css', '.scss', '.html', '.json']
+        extensions: ['.ts', '.js', '.css', '.scss', '.html', '.json']
     };
     config.optimization = {
-        minimizer: [new TerserJSPlugin({}),
+        minimizer: [
+            new TerserJSPlugin({}),
             new OptimizeCSSAssetsPlugin({})
         ],
     };
@@ -31,6 +35,7 @@ module.exports = function init() {
             chunkFilename: '[id].scss'
         })
     ];
+    config.watch = true;
     config.module = {
         rules: [{
             test: /\.js$/,
@@ -41,6 +46,11 @@ module.exports = function init() {
             }
         },
         {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: path.resolve(__dirname, 'node_modules')
+        },
+        {
             test: /\.scss$/,
             use: [
                 MiniCssExtractPlugin.loader, {
@@ -49,19 +59,21 @@ module.exports = function init() {
                         sourceMap: true,
                         importLoaders: 1
                     }
-                }, {
-                    loader: 'sass-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                }, {
+                },
+                {
                     loader: 'postcss-loader',
                     options: {
                         config: {
                             path: './postcss.config.js'
                         }
                     }
-                }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
             ],
         },
         {
@@ -72,7 +84,9 @@ module.exports = function init() {
     };
     return config;
 };
+
 // Helper functions
+// eslint-disable-next-line no-unused-vars
 function root(args) {
     args = Array.prototype.slice.call(arguments, 0);
     return path.join.apply(path, [__dirname].concat(args));
